b48793bc5be2677f05ba86957877a25d8ec1e7fd
RHartshorn <hartshorn.robin@gmail.com>
2016-02-25 13:55:09 -0800

    copying lab4 to Exam1 to form the base of Exam work

---
+++ Exam1TakeHome/io.h
@@ -0,0 +1,18 @@
+#ifndef IO_H
+#define IO_H
+
+#define BASE8 8;
+#define BASE10 10;
+#define BASE16 16;
+
+char *ascciTable = "0123456789ABCDEF";
+
+//Input
+
+//Output
+char *gets(char str[]);
+
+//Utility
+int atoi(char *str);
+
+#endif
